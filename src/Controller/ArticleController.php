2559514d1b780e5025ea8a402e382e103832bb2a
RSHKDL <julien.helfer@gmail.com>
2019-11-07 14:34:20 +0100

    install twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class ArticleController
  * @author ereshkidal
  */
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -26,9 +27,15 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article: "%s"',
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
