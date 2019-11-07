3b999e6019d161e63b7d6d85f8d01784ce77e891
RSHKDL <julien.helfer@gmail.com>
2019-11-07 14:04:42 +0100

    install annotations

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ArticleController
+ * @author ereshkidal
+ */
+class ArticleController
+{
+
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function homepage()
+    {
+        return new Response('Hello world ;)');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     * @return Response
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+}
