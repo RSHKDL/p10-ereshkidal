deb419f0a32616993043bd373283640ef4532e26
RSHKDL <julien.helfer@gmail.com>
2019-11-20 16:36:10 +0100

    install knp_paginator

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\CommentRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,13 +26,21 @@ class AdminController extends AbstractController
      * @param CommentRepository $repository
      * @return Response
      */
-    public function manageComments(CommentRepository $repository, Request $request): Response
-    {
+    public function manageComments(
+        CommentRepository $repository,
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response {
         $term = $request->query->get('q');
-        $comments = $repository->findAllWithFilter($term);
+        $queryBuilder = $repository->getQueryBuilderWithFilter($term);
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1),
+            10
+        );
 
         return $this->render('admin/comments.html.twig', [
-            'comments' => $comments
+            'pagination' => $pagination
         ]);
     }
 }
